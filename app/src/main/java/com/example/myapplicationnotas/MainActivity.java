d0bb3341cdfce7b89bc196e3290f512a67c51082
Diego Acosta <diegoalexac@gmail.com>
2023-07-16 23:15:20 -0500

    commit first

---
+++ app/src/main/java/com/example/myapplicationnotas/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplicationnotas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSumar;
+    EditText txtNumero1;
+    EditText txtNumero2;
+    TextView txtResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnSumar = findViewById(R.id.btnSumar);
+        txtNumero1 = findViewById(R.id.txtNumero1);
+        txtNumero2 = findViewById(R.id.txtNumero2);
+        txtResultado = findViewById(R.id.txtResultado);
+    }
+        public void sumar (View view){
+            int num1 = Integer.parseInt(txtNumero1.getText().toString());
+            int num2 = Integer.parseInt(txtNumero2.getText().toString());
+            int res = num1 + num2;
+            txtResultado.setText(""+res);
+
+
+
+    }
+}
